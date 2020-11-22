7c1ff325a3b4c491db83da41abf5b4e263c9c4a5
thanakorn <thanakorn.panyapiang@outlook.com>
2020-11-22 13:44:43 +0700

    Update config

--- configs/luna/ssd300_luna.py
+++ configs/luna/ssd300_luna.py
@@ -3,7 +3,17 @@ input_size = 512
 model = dict(
     pretrained=None,
     backbone=dict(input_size=input_size),
-    bbox_head=dict(num_classes=1))
+    bbox_head=dict(
+      num_classes=1,
+      in_channels=(512, 1024, 512, 256, 256, 256, 256),
+      anchor_generator=dict(
+            type='SSDAnchorGenerator',
+            scale_major=False,
+            input_size=input_size,
+            basesize_ratio_range=(0.1, 0.9),
+            strides=[8, 16, 32, 64, 128, 256, 512],
+            ratios=[[2], [2, 3], [2, 3], [2, 3], [2, 3], [2], [2]])
+    ))
 
 # dataset settings
 dataset_type = 'CocoDataset'
