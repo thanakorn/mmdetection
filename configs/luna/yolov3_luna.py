abb4f259cddcaf22ab55a2b4f258df6b9b2948ba
Thanakorn Panyapiang <thanakorn.panyapiang@outlook.com>
2020-12-06 23:32:54 +0700

    Update yolov3_luna.py

--- configs/luna/yolov3_luna.py
+++ configs/luna/yolov3_luna.py
@@ -5,30 +5,55 @@ model = dict(
     bbox_head=dict(num_classes=1)
   )
 
+img_norm_cfg = dict(mean=[0, 0, 0], std=[255., 255., 255.], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='PhotoMetricDistortion'),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 2)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
+        min_crop_size=0.3),
+    dict(type='Resize', img_scale=[(320, 320), (608, 608)], keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+
 # dataset settings
-dataset_type = 'COCODataset'
+dataset_type = 'CocoDataset'
 data_root = 'LUNA/'
 classes = ('nodule',)
 
 data = dict(
     train=dict(
-          img_prefix='LUNA/',
-          classes=classes,
-          ann_file='LUNA/train.json'
+      type='RepeatDataset',
+      times=5,
+        dataset=dict(
+            type=dataset_type,
+            img_prefix='LUNA/',
+            classes=classes,
+            ann_file='LUNA/train.json',
+            pipeline=train_pipeline)
         ),
     val=dict(
         img_prefix='LUNA/',
         classes=classes,
-        ann_file='LUNA/train.json'
-      ),
+        ann_file='LUNA/train.json'),
     test=dict(
         img_prefix='LUNA/',
         classes=classes,
-        ann_file='LUNA/train.json'
-      ))
+        ann_file='LUNA/train.json'))
 
 optimizer = dict(type='SGD', lr=0.001)
 
-log_config = dict(interval=10)
-evaluation = dict(interval=20)
+log_config = dict(interval=60)
+evaluation = dict(interval=25)
 total_epochs = 20
